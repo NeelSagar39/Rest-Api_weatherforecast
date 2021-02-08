5005d0f9bbe9d98ce76f6e42018f2d73c1259522
NeelSagar39 <neelsagar39@gmail.com>
2021-02-08 06:51:52 +0530

    Added rainfall-prediction

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ import model
 app = Flask(__name__)
 api = Api(app)
 
-class test_ex(Resource):
+class temp_res(Resource):
     def get(self):
         historical_data = data.request_hourly()
-        j_pred = model.train_model(historical_data)
+        j_pred = model.train_temp_model(historical_data)
         return j_pred
-api.add_resource(test_ex,"/get-predictions")
+api.add_resource(temp_res,"/get-temp-predictions")
+
+class rain_res(Resource):
+    def get(self):
+        historical_data = data.request_hourly()
+        j_pred = model.train_rainfall_model(historical_data)
+        return j_pred
+api.add_resource(rain_res,"/get-rainfall-predictions")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- model.py
+++ model.py
@@ -5,28 +5,65 @@ import json
 import time
 import datetime
 import math
-from statsmodels.tsa.ar_model import AutoReg
-from sklearn.metrics import mean_squared_error
+
     
 
 def pre_process(historical_data):
     temp = []
+    rainfall = []
     for d in historical_data:
         for h in d:
             temp.append(dict(h)['temp']-273.15) 
-    return temp
-def train_model(historical_data):
-    temp = pre_process(historical_data)
-    train = temp
-    print(temp[-5:])
-    #test = temp[75::]
+            rainfall.append(dict(h)['weather'])
+    return temp,rainfall
+def train_temp_model(historical_data):
+    train,rainfall = pre_process(historical_data)
+    from statsmodels.tsa.ar_model import AutoReg
+    from sklearn.metrics import mean_squared_error
     model_ar_fit = AutoReg(train, lags=[6,12,24,48]).fit()
-    predictions = model_ar_fit.predict(start = len(temp),end=len(temp)+24)
+    predictions = model_ar_fit.predict(start = len(train),end=len(train)+24)
     proper_data = []
     for i in predictions:
         new_i = math.floor(i)
         proper_data.append(new_i)
-    #print(test, predictions)
-    #print(mean_squared_error(y_pred=predictions,y_true=test))
     json_predictions = dict(enumerate(proper_data))
     return json_predictions
+
+
+def train_rainfall_model(historical_data):
+    temp,train = pre_process(historical_data)
+    from statsmodels.tsa.ar_model import AutoReg
+    from sklearn.metrics import mean_squared_error
+    rainfall_data = []
+    rain_final = []
+    for i in train:
+        print(i)
+        temp = dict(i[0])
+        rainfall_data.append(temp['main'])
+    print(np.unique(rainfall_data))
+    for j in rainfall_data:
+        if j == 'Clouds':
+            rain_final.append(0)
+        elif j == 'Drizzle':
+            rain_final.append(0.50)
+        elif j == 'Rain':
+            rain_final.append(1)
+    model_ar_fit = AutoReg(rain_final, lags=[6,12,24,48]).fit()
+    predictions = model_ar_fit.predict(start = len(rain_final),end=len(rain_final)+24)
+    json_predictions = dict(enumerate(predictions))
+    return json_predictions
+
+# def train_model(historical_data):
+#     temp,rainfall = pre_process(historical_data)
+#     rain_data = train_rainfall_model(rainfall)
+#     train = temp
+#     print(temp[-5:])
+#     json_predictions = train_temp_model(train)
+#     # model_ar_fit = AutoReg(train, lags=[6,12,24,48]).fit()
+#     # predictions = model_ar_fit.predict(start = len(temp),end=len(temp)+24)
+#     # proper_data = []
+#     # for i in predictions:
+#     #     new_i = math.floor(i)
+#     #     proper_data.append(new_i)
+#     # json_predictions = dict(enumerate(proper_data))
+#     return json_predictions
